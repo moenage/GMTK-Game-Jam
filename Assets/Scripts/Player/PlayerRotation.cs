d130bc3797b6fa14e39d26a35c247aa1c2c6643e
moenage <moenage7@yahoo.com>
2023-07-07 06:39:43 -0700

    Testing

--- Assets/Scripts/Player/PlayerRotation.cs
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
-    // Update is called once per frame
     void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
