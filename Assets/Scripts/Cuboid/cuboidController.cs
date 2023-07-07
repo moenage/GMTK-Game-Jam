bdd7fb763d249da5a1ebedfd9e1304e8c3d587a2
moenage <moenage7@yahoo.com>
2023-07-07 13:54:41 -0700

    Tried adding enemy shooting,, needs work

--- Assets/Scripts/Cuboid/cuboidController.cs
+++ Assets/Scripts/Cuboid/cuboidController.cs
@@ -7,6 +7,37 @@ public class cuboidController : MonoBehaviour
 
     public int hitPoints;
 
+    public GameObject playerTarget;
+    public float cuboidSpeed;
+
+    public float cuboidTimedShots;
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private float distance;
+    private bool canShoot;
+
+    private void Start() {
+        canShoot = true;
+    }
+
+    private void Update() {
+        if (canShoot) {
+            cuboidFire();
+        }
+    }
+
+    private void FixedUpdate() {
+        distance = Vector2.Distance(transform.position, playerTarget.transform.position);
+        Vector2 direction = playerTarget.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x)  * Mathf.Rad2Deg;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, playerTarget.transform.position, cuboidSpeed * Time.fixedDeltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.tag == "Bullet") {
@@ -16,5 +47,18 @@ public class cuboidController : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+    }
+
+    public void cuboidFire() {
+        canShoot = false;
+
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        StartCoroutine(cuboidShotCooldwon());
+    }
+
+    IEnumerator cuboidShotCooldwon() {
+        yield return new WaitForSeconds(cuboidTimedShots);
+        canShoot = true;
     }
 }
