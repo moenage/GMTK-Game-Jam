c21d9a24ad3fc13573b61296917004d6b52f6ac0
moenage <moenage7@yahoo.com>
2023-07-09 00:34:05 -0700

    Added health bar and collision tracking for player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     public float moveSpeed = 5f;
+
+    public Image healthBar;
+    public float healthAmount;
 
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -22,5 +26,12 @@ public class PlayerController : MonoBehaviour {
         movement = new Vector2(horizontalInput, verticalInput) * moveSpeed;
         // Apply movement to the rigidbody
         rb.velocity = movement;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "EnemyBullet" || collision.gameObject.tag == "Cuboid" || collision.gameObject.tag == "Kamikaze") {
+            healthAmount = healthAmount - 10;
+            healthBar.fillAmount = healthAmount / 100f;
+        }
     }
 }
